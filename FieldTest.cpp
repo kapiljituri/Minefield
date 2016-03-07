12cea2144d6f591111e777050382dc29c2864673
kapiljituri <kapiljituri@gmail.com>
2016-03-07 10:24:17 -0800

    Implemented isSafe() function

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "Field.h"
- 
+
 class FieldTest : public ::testing::Test
 {
 	protected:
@@ -18,7 +18,19 @@ class FieldTest : public ::testing::Test
 TEST(FieldTest, placeMineInBounds)
 {
 	Field minefield;
-	
+
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
+}
+
+TEST(FieldTest, isSafeTest)
+{
+	Field minefield;
+  minefield.placeMine(0,9);
+
+  try{
+	   ASSERT_TRUE( minefield.isSafe(9,9) );
+   } catch (std::string x){
+        std::cout << "Out of Bound Exception!" << std::endl;
+   }
 }
